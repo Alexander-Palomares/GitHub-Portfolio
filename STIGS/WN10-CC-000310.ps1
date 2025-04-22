ba6ee563c7cbedadc893e9e25112636b22436a92
Alexander Palomares <123430301+Alexander-Palomares@users.noreply.github.com>
2025-04-22 14:51:53 -0500

    Create WN10-CC-000310.ps1

---
+++ STIGS/WN10-CC-000310.ps1
@@ -0,0 +1,26 @@
+<#
+.SYNOPSIS
+    
+   
+.NOTES
+    Author          : Alexander Palomares
+    LinkedIn        : linkedin.com/in/alexander-palomares-a867202b1/ 
+    GitHub          : github.com/Alexander-Palomares
+    Date Created    : 2025-04-22
+    Last Modified   : 2025-04-22
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000310
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-CC-000310.ps1 
+#>
