f25b542d572849fac4dacaab8889f40e3b550a3b
Alexander Palomares <123430301+Alexander-Palomares@users.noreply.github.com>
2025-04-22 13:44:37 -0500

    Update WN10-CC-000175.ps1

--- STIGS/WN10-CC-000175.ps1
+++ STIGS/WN10-CC-000175.ps1
@@ -1,3 +1,31 @@
+<#
+.SYNOPSIS
+    Disables Windows compatibility telemetry inventory collection by setting the 'DisableInventory' registry value as required by STIG compliance.
+   
+
+.NOTES
+    Author          : Alexander Palomares
+    LinkedIn        : linkedin.com/in/alexander-palomares-a867202b1/ 
+    GitHub          : github.com/Alexander-Palomares
+    Date Created    : 2025-04-22
+    Last Modified   : 2025-04-22
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000175
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-CC-000175.ps1 
+#>
+
 # Define the registry path
 $regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\AppCompat"
 
